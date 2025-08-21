6b281eb44aa1720eb713483e9be3596a9ef54c8b
DanXploitYT <danielcrismercurio0@gmail.com>
2025-08-21 21:50:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content.toLowerCase() === "growagarden") {
+    message.channel.send("grow a garden script made by : daniel");
+  }
+});
+
+client.login(process.env.TOKEN);
